078bbe939909cd0b755b5a5e17817728191d0ac2
Evgenia0712 <e.gunko1992@mail.ru>
2023-06-04 18:07:07 +0300

    project

---
+++ navigation.js
@@ -0,0 +1,15 @@
+let time;
+time = 34;
+console.log(time);
+console.log('Старт поездки. Осталось ехать ' + time + ' минут');
+time -= 15;
+//console.log(time);
+console.log('Немного сторис в соцсетях. Осталось ехать ' + time + ' минут');
+time -= 10;
+//console.log(time);
+console.log('Немного не новостей, но событий. Осталось ехать ' + time + ' минут');
+time = 0;
+//console.log(time);
+console.log('Вы приехали. Добро пожаловать в Москву!');
+
+
